3c80e08cbda07711c640d5c51183c31f501fdde3
Jacob Gaisinsky <jacob.gaisinsky@gmail.com>
2023-01-19 22:08:15 -0500

    acid fixed

--- Fireboy and Watergirl FSE/Assets/Scrpts/PlayerStats.cs
+++ Fireboy and Watergirl FSE/Assets/Scrpts/PlayerStats.cs
@@ -3,8 +3,8 @@ public static class PlayerStats
     public static int level { get; set; } = 0;
     public static int fire_gems { get; set; } = 0;
     public static int water_gems { get; set; } = 0;
-    public static int[] fire_gems_per_level { get; set; } = {0, 2, 11, 4, 8, 8, 0, 0, 0, 0};
-    public static int[] water_gems_per_level { get; set; } = {0, 3, 11, 5, 8, 8, 0, 0, 0, 0};
+    public static int[] fire_gems_per_level { get; set; } = {0, 2, 11, 4, 8, 8, 6, 0, 0, 0};
+    public static int[] water_gems_per_level { get; set; } = {0, 3, 11, 5, 8, 8, 6, 0, 0, 0};
 
     public static void reset_player_stats() {
         fire_gems = 0;
