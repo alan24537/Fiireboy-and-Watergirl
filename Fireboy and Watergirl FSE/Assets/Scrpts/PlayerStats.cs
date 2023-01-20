301c4b3c8b94b494690903eec0a39b9c8ab71f71
Jacob Gaisinsky <jacob.gaisinsky@gmail.com>
2023-01-20 10:36:21 -0500

    added music and grey diamonds

--- Fireboy and Watergirl FSE/Assets/Scrpts/PlayerStats.cs
+++ Fireboy and Watergirl FSE/Assets/Scrpts/PlayerStats.cs
@@ -6,7 +6,7 @@ public static class PlayerStats
     public static int special_gems { get; set; } = 0;
     public static int[] fire_gems_per_level { get; set; } = {0, 2, 11, 4, 8, 8, 6, 0, 0, 0};
     public static int[] water_gems_per_level { get; set; } = {0, 3, 11, 5, 8, 8, 6, 0, 0, 0};
-    public static int[] special_gems_per_level { get; set; } = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static int[] special_gems_per_level { get; set; } = {0, 0, 0, 0, 0, 0, 0, 1, 1, 0};
 
     public static void reset_player_stats() {
         fire_gems = 0;
