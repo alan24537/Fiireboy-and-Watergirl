02f4231994dc80e220b7d2066431e4519592ab3b
Jacob Gaisinsky <jacob.gaisinsky@gmail.com>
2023-01-19 22:57:04 -0500

    moreeee tiles
    :bruh:

--- Fireboy and Watergirl FSE/Assets/Scrpts/SceneMover.cs
+++ Fireboy and Watergirl FSE/Assets/Scrpts/SceneMover.cs
@@ -8,21 +8,14 @@ public class SceneMover : MonoBehaviour {
     public static int MENU_SCENE = 0;
     public static int DEATH_SCENE = 1;
     public static int CONTINUE_SCENE = 2;
-    public static int LEVEL_ONE = 3;
 
     public void playGame() {
 
         // Reset the player stats
         PlayerStats.reset_player_stats();
 
-        if (PlayerStats.level == 0) {
-            PlayerStats.level = 1;
-            SceneManager.LoadScene(LEVEL_ONE);
-        }
-        else {
-            PlayerStats.level ++;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+        PlayerStats.level ++;
+        SceneManager.LoadScene(PlayerStats.level + 2);
     }
 
     public void goToMenu() {
@@ -34,6 +27,6 @@ public class SceneMover : MonoBehaviour {
     }
 
     public void retryGame() {
-        SceneManager.LoadScene(DEATH_SCENE);
+        SceneManager.LoadScene(PlayerStats.level + 2);
     }
 }
